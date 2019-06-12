8c01f52232bffbd84c1450f0ed9041af384d6714
Haja-andri <haja.andri@gmail.com>
2019-06-12 14:41:54 +0200

    created App class, mounting data from native methode fetch

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          HTTP / AJAX Project
-        </p>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           HTTP / AJAX Project here
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+export default class App extends React.Component {
+
+  state = {
+    friendsData: [],
+    errorMessage: ''
+  }
+
+  fetchFriendsData = () => {
+    fetch('http://localhost:5000/friens')
+  .then(response =>{
+    return response.json();
+  })
+  .then(friends => {
+    this.setState({
+      friendsData: friends,
+    });
+  })
+  .catch(error => {
+    this.setState({
+      errorMessage: error.message,
+    })
+  })
+
+  }
+
+  componentDidMount (){
+    this.fetchFriendsData();
+  }
+
+
+
+  render () {
+    return (
+      <div className="App-header">Something here</div>
+    );
+  }
+}
