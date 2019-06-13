00b34fc701eec02ca3f6f10507bde0de707225a0
Haja-andri <haja.andri@gmail.com>
2019-06-13 16:43:10 +0200

    Updated the Form to move to Form + OnSubmit instead of OnClick

--- friends/src/Components/Form/Form.js
+++ friends/src/Components/Form/Form.js
@@ -3,18 +3,21 @@ import React from 'react';
 export default function InputForm (props) {
     const { newFriendName, newFriendAge, newFriendEmail, inputHandler, addNewFriend, currentMode} = props;
     return(
+
+        <form onSubmit={addNewFriend}>
         <div className="form-countainer">
-        <div className="inputs-section">
-          <label>Name</label> 
-          <input value={newFriendName} onChange = {inputHandler} name="name" type='text'></input>
-          <label>Age</label> 
-          <input value={newFriendAge} onChange = {inputHandler} name="age" type='text'></input>
-          <label>Email</label> 
-          <input value={newFriendEmail} onChange = {inputHandler} name="email" type='text'></input>
+            <div className="inputs-section">
+            <label>Name</label> 
+            <input value={newFriendName} onChange = {inputHandler} name="name" type='text'></input>
+            <label>Age</label> 
+            <input value={newFriendAge} onChange = {inputHandler} name="age" type='text'></input>
+            <label>Email</label> 
+            <input value={newFriendEmail} onChange = {inputHandler} name="email" type='text'></input>
+            </div>
+            <div className="button-section">
+            <button type="submit">{currentMode} Friends</button>
+            </div>
         </div>
-        <div className="button-section">
-          <button onClick={addNewFriend} >{currentMode} Friends</button>
-        </div>
-      </div>
+        </form>
     );
 } 
