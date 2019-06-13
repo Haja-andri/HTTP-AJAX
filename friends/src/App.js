1c392ccb800ca4d9cf9c3b3e402e5fc852c564ae
Haja-andri <haja.andri@gmail.com>
2019-06-13 09:32:22 +0200

    Adding new friend locally - post to come later

--- friends/src/App.js
+++ friends/src/App.js
@@ -6,7 +6,10 @@ export default class App extends React.Component {
 
   state = {
     friendsData: null,
-    errorMessage: ''
+    errorMessage: '',
+    newFriendName: '',
+    newFriendAge: '',
+    newFriendEmail: '',
   }
 
   fetchFriendsData = () => {
@@ -36,8 +39,36 @@ export default class App extends React.Component {
       friend.id !== Number(friendId)
     ));
     this.setState({
+      // Probably have to post here
       friendsData: newFriendsData,
     })
+  }
+
+  // addFriend = () => {
+  //   console.log('adding friend')
+  // }
+
+  inputHandler = (event) => {
+    if(event.target.name === 'name') this.setState({ newFriendName: event.target.value });
+    else if (event.target.name === 'age') this.setState({ newFriendAge: event.target.value });
+    else if (event.target.name === 'email') this.setState({ newFriendEmail: event.target.value });
+  }
+
+  addNewFriend = () => {
+    const newFriend = {
+      id: this.state.friendsData.length + 1,
+      name: this.state.newFriendName,
+      age: this.state.newFriendAge,
+      email: this.state.newFriendEmail,
+    };
+    
+    this.state.friendsData.push(newFriend);
+
+    this.setState({
+      friendsData: this.state.friendsData,
+      // Probably have to post here
+    })
+
   }
 
   render () {
@@ -45,12 +76,15 @@ export default class App extends React.Component {
       <>
       <div className="form-countainer">
         <div className="inputs-section">
-          <label>Name</label> <input type="text" />
-          <label>Age</label> <input type="text" />
-          <label>Email</label> <input type="text" />
+          <label>Name</label> 
+          <input value ={this.state.name} onChange = {this.inputHandler} name="name" type='text'></input>
+          <label>Age</label> 
+          <input value ={this.state.age} onChange = {this.inputHandler} name="age" type='text'></input>
+          <label>Email</label> 
+          <input value ={this.state.email} onChange = {this.inputHandler} name="email" type='text'></input>
         </div>
         <div className="button-section">
-          <button>Add Friends</button>
+          <button onClick={this.addNewFriend} >Add Friends</button>
         </div>
       </div>
       <div className="friends-container">
--- friends/src/Components/FriendsList.js
+++ friends/src/Components/FriendsList.js
@@ -12,7 +12,7 @@ export default function FriendsList (props) {
             {
             friendsData.map(friend =>(
                 <div className="friend-box" key={friend.id}>
-                <div className="delete" onClick={deleteThisFriend} id={friend.id}>X</div>
+                <div className="delete" onClick={deleteThisFriend} id={friend.id}>x</div>
                 <div className="friend-header">{friend.name} <span onClick="">| Edit</span></div>
                 <div className="line-wrapper"><label>Age :</label> <div>{friend.age}</div></div>
                 <div className="line-wrapper"><label>email :</label> <div>{friend.email}</div></div>        
